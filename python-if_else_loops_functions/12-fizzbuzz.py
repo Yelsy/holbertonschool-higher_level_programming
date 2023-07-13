f9d51c9b92be96826126ae463c7e33cc89ea5ad4
yelsy <yelsycg17@gmail.com>
2023-07-12 22:47:53 -0700

    printing multiples fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if n % 5 == 0 and n % 3 == 0:
             print("FizzBuzz ", end="")
         elif n % 3 == 0:
-            print("Buzz ", end="")
-        elif n % 5 == 0:
             print("Fizz ", end="")
+        elif n % 5 == 0:
+            print("Buzz ", end="")
         else:
             print("{} ".format(n), end="")
