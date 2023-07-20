96668eee7b055fb4d35ae47674d5a89662d436d0
yelsy <yelsycg17@gmail.com>
2023-07-20 15:09:37 -0700

    returns a tuple with the length of a string

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    count = len(sentence)
+    if count == 0:
+        first = None
+        return count, first
+    else:
+        first = sentence[0]
+        return count, first
