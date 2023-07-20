75a88972ac829f8ad70570b6b28267ec115ba511
yelsy <yelsycg17@gmail.com>
2023-07-20 13:15:33 -0700

    prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        i = 0
+        for j in row:
+            len_matrix = len(row)
+            i += 1
+            print("{:d}".format(j), end="")
+            if i != len_matrix:
+                print(" ", end="")
+        print("")
