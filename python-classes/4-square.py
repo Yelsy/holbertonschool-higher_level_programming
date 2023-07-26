33f38474aabe0fb2f712fdc304989fa4d2a546c0
yelsy <yelsycg17@gmail.com>
2023-07-26 14:32:04 -0700

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""size validation"""
+
+
+class Square:
+    """class variable size"""
+
+    def __init__(self, size=0):
+        """initialize size"""
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """public instance method that returns area"""
+
+        return self.__size * self.__size
