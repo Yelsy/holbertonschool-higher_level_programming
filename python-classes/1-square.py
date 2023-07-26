edccb5c24712d8bb9597c04960e4c85deea95174
yelsy <yelsycg17@gmail.com>
2023-07-26 13:23:04 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" private instance attribute"""
+
+
+class Square:
+    """ class variabel size"""
+
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
