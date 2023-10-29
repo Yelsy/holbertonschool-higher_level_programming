51ad9537c81e25f820816c34d9ce9480993d694d
yelsy <yelsycg17@gmail.com>
2023-10-28 21:24:03 -0500

    feat: update the text of the header element to New Headergit add 6-script.js !

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Add a click event handler to the DIV
+    $('#update_header').click(function() {
+      // Select the header element using the header and update its text
+      $('header').text('New Header!!!');
+    });
+  });
