0769544066ddffa51ef5e28751eb4cf1e4631c5a
yelsy <yelsycg17@gmail.com>
2023-10-28 21:48:25 -0500

    feat:displays the value of hello from that fetch in the HTML tag

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,8 +1,10 @@
+// Send an AJAX request to the specified URL using the jQuery $.ajax method
 $.ajax({
     url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
     method: 'GET',
     dataType: 'json',
     success: function(data) {
+      // Display the translation in the div with the ID hello
       $('#hello').text(data.hello);
     },
     error: function() {
